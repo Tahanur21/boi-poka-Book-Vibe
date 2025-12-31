635a74ba22a0cd7041f3e42b9afe04fc6a91ab0b
Tahanur Islam <tahanur373@gmail.com>
2025-12-31 13:05:24 +0600

    third comment

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+
+const Root = () => {
+    return (
+        <div>
+            <NavBar/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Root;
